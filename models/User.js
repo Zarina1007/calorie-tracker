ae48f23fda038813ea3c1fce3cb478d5bd3aa22f
Mohit Sengar <mohitsingh.shiv@gmail.com>
2024-06-27 01:23:20 +0000

    update

--- models/User.js
+++ models/User.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
-const { DateTime } = require('luxon');
+
+// Function to add ordinal suffix
+function getOrdinalSuffix(day) {
+    const j = day % 10,
+        k = day % 100;
+    if (j == 1 && k != 11) {
+        return 'st';
+    }
+    if (j == 2 && k != 12) {
+        return 'nd';
+    }
+    if (j == 3 && k != 13) {
+        return 'rd';
+    }
+    return 'th';
+}
 
 const userSchema = new mongoose.Schema({
     id: { type: Number, required: true, unique: true },
@@ -12,7 +27,21 @@ const userSchema = new mongoose.Schema({
 userSchema.methods.toJSON = function() {
     const obj = this.toObject();
     if (obj.birthday) {
-        obj.birthday = DateTime.fromJSDate(obj.birthday).toFormat('MMMM dd, yyyy');
+        const date = new Date(obj.birthday);
+
+        // Using Intl.DateTimeFormat for month and year
+        const options = { month: 'long', year: 'numeric' };
+        const formatter = new Intl.DateTimeFormat('en-US', options);
+        const parts = formatter.formatToParts(date).reduce((acc, part) => {
+            acc[part.type] = part.value;
+            return acc;
+        }, {});
+        
+        const day = date.getDate();
+        const month = parts.month;
+        const year = parts.year;
+
+        obj.birthday = `${month} ${day}${getOrdinalSuffix(day)}, ${year}`;
     }
     return obj;
 };
