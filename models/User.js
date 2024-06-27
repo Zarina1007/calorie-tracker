f9b74b931c7938ca40f9d99265a479f9d431b923
Mohit Sengar <mohitsingh.shiv@gmail.com>
2024-06-27 01:13:09 +0000

    id update

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const { DateTime } = require('luxon');
 
 const userSchema = new mongoose.Schema({
-    id: { type: String, required: true, unique: true },
+    id: { type: Number, required: true, unique: true },
     first_name: { type: String, required: true },
     last_name: { type: String, required: true },
     birthday: { type: Date, required: true },
